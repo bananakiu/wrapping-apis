a9e026982a9a7b71cd9840adf14ad5ea110b3859
bananakiu <leandrenoelkiu@gmail.com>
2021-12-02 18:39:49 +0800

    remove home route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'pages#main'
+  root to: 'pages#tags'
   # resources :images
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "main" => "pages#main", as: "main"
+  # get "main" => "pages#main", as: "main"
   # get "result" => "pages#result", as: result_path
   get "tags" => "pages#tags", as: "tags"
   get "categories" => "pages#categories", as: "categories"
