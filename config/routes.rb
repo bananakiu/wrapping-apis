422bf6c9f9f94e7670f635cd12c76a21a16b8689
bananakiu <leandrenoelkiu@gmail.com>
2021-12-02 15:03:08 +0800

    design basic website layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'pages#main'
   # resources :images
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "main" => "pages#main"
-  get "result" => "pages#result"
-  get "tags" => "pages#tags"
-  get "categories" => "pages#categories"
-  get "categorize" => "pages#categorize"
-  get "detect_text" => "pages#detect_text"
+  get "main" => "pages#main", as: "main"
+  # get "result" => "pages#result", as: result_path
+  get "tags" => "pages#tags", as: "tags"
+  get "categories" => "pages#categories", as: "categories"
+  get "categorize" => "pages#categorize", as: "categorize"
+  get "detect_text" => "pages#detect_text", as: "detect_text"
   # get "upload" => "pages#upload"
 end
